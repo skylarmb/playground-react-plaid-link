97e1e47938b493b895e14f78392a3bd06550de85
Skylar Brown <sbrown@plaid.com>
2021-08-25 19:02:31 -0700

    OAuth demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,76 @@
 import React from "react";
+import {
+  usePlaidLink,
+  PlaidLinkOptions,
+  PlaidLinkOnSuccess,
+} from "react-plaid-link";
 
-import "./App.css";
+const App: React.FC = () => {
+  const isOauthRedirect = document.location.href.includes("oauth_state_id");
 
-import Link from "./Link";
-
-interface Props {}
-
-const App: React.FC<Props> = (props: Props) => {
   const [token, setToken] = React.useState<string | null>(null);
 
   const generateToken = async () => {
     const response = await fetch("/api/create_link_token", {
-      method: "POST"
+      method: "POST",
     });
     const data = await response.json();
     setToken(data.link_token);
+    sessionStorage.setItem("link_token", data.link_token);
   };
 
+  React.useEffect(() => {
+    if (isOauthRedirect) {
+      setToken(sessionStorage.getItem("link_token"));
+    } else {
+      generateToken();
+    }
+  }, [isOauthRedirect]);
+
+  return token == null ? (
+    <div className="loader"></div>
+  ) : (
+    <LinkButton token={token} isOauthRedirect={isOauthRedirect} />
+  );
+};
+
+interface ButtonProps {
+  token: string;
+  isOauthRedirect: boolean;
+}
+
+const LinkButton: React.FC<ButtonProps> = (props: ButtonProps) => {
+  const onSuccess = React.useCallback<PlaidLinkOnSuccess>(
+    (public_token, metadata) => {
+      // send public_token to server
+      console.log("success: public_token", public_token);
+    },
+    []
+  );
+
+  const config: PlaidLinkOptions = {
+    token: props.token,
+    onSuccess,
+  };
+
+  // if we are handling an OAuth redirect, add receivedRedirectUri to the config
+  if (props.isOauthRedirect) {
+    config.receivedRedirectUri = document.location.href;
+  }
+
+  const { open, ready, error } = usePlaidLink(config);
+
+  // if handling OAuth, open Link immediately instead of requiring button click
+  React.useEffect(() => {
+    if (props.isOauthRedirect && ready) {
+      open();
+    }
+  }, [props.isOauthRedirect, open, ready]);
+
   return (
-    <>
-      <button onClick={generateToken}>
-        generate token on click and then open Link
-      </button>
-      {token != null && <Link token={token} />}
-    </>
+    <button onClick={() => open()} disabled={!ready}>
+      open link
+    </button>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import App2 from "./App2";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <br />
-    <App2 />
   </React.StrictMode>,
   document.getElementById("root")
 );
